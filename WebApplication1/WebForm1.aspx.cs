7e19b68d7fa00faafb540e5a5cd429703b7468f7
feng.li <lifeng0901@126.com>
2018-02-26 16:52:40 +0800

    aa

--- WebApplication1/WebForm1.aspx.cs
+++ WebApplication1/WebForm1.aspx.cs
@@ -13,8 +13,27 @@ namespace WebApplication1
         {
             using (ClassLibrary1.InspectionWHEntities1 db = new InspectionWHEntities1())
             {
-                WorkOrderBaseInfo a = db.WorkOrderBaseInfo.FirstOrDefault();
-                Response.Write(a.WorkId);
+                //var trans = db.Database.BeginTransaction();
+                try
+                {
+                    WorkOrderBaseInfo a = new WorkOrderBaseInfo();
+                    a.InfoID = Guid.NewGuid();
+                    a.WorkId = "test33333333-123";
+                    WorkOrderBaseInfo b = new WorkOrderBaseInfo();
+                    b.InfoID = Guid.NewGuid();
+                    b.WorkId = "test4444444-123222222222222222222222222222222222222222222222222222222222222222222222222222222222222222";
+                    db.WorkOrderBaseInfo.Add(a);
+                    db.WorkOrderBaseInfo.Add(b);
+                    db.SaveChanges();
+                  //  trans.Commit();
+                    Response.Write(a.WorkId + b.WorkId);
+                }
+                catch (Exception ex)
+                {
+                    Response.Write(ex.Message);
+                   // trans.Rollback();
+                }
+                
             }
                
         }
